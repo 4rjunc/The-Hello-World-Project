2e2bcc15746d65ac755e04af0bcef98d200fe5da
Ritik Shrestha <66729712+shresritik@users.noreply.github.com>
2023-10-04 21:01:22 +0545

    Added HelloWorld in Fortran

---
+++ HelloWorld.f95
@@ -0,0 +1,12 @@
+! 
+! 
+!                             Online Fortran Compiler.
+!                 Code, Compile, Run and Debug Fortran program online.
+! Write your code in this editor and press "Run" button to execute it.
+! 
+! 
+
+
+Program Hello
+Print *, "Hello World"
+End Program Hello
